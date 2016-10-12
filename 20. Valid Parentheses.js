fab69047c31d7ed686a6d0519d3f413c6ec3fd46
余果 <fishguo321@gmail.com>
2016-10-13 01:05:49 +0800

    第一次写的时候喝醉了吧……优化了一下

--- 20. Valid Parentheses.js
+++ 20. Valid Parentheses.js
@@ -3,36 +3,25 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    // "" -> return true
-    // s.length === odd -> return false
-    if(s.length === 0) return true;
-    if(s.length % 2 !== 0) return false;
-    
     var map = {
-        '(': 1,
-        ')': -1,
-        '[': 10,
-        ']': -10,
-        '{': 100,
-        '}': -100
+        '(': ')',
+        '[': ']',
+        '{' : '}'
     }
     
-    var array = s.split(''),
-        stack = [],
-        result = false;
-        
-    array.forEach(char =>{
-        var currValue = map[char];
-        if(currValue > 0){
-            stack.push(currValue);
-        }else if(stack[stack.length - 1] === -currValue){
-            stack.pop();
+    var stack = [],
+        sLength = s.length;
+      
+    for(var i = 0; i < sLength; i++){
+        var char = s[i];
+        if(map[char]){
+            stack.push(map[char]);
         }else{
-            return false;
+            if(char !== stack.pop()){
+                return false;
+            }
         }
-    });
-    
-    if(stack.length === 0){result = true}
-    
-    return result;
+    }  
+     
+    return stack.length === 0;
 };
