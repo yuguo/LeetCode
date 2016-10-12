37e6b2cd8cddd03394dfd77e3306974b8ad99146
余果 <fishguo321@gmail.com>
2016-10-13 01:46:02 +0800

    去掉了无意义的判断，代码更加简洁

--- 21. Merge Two Sorted Lists.js
+++ 21. Merge Two Sorted Lists.js
@@ -21,18 +21,10 @@ var mergeTwoLists = function(l1, l2) {
     
     if(l1.val < l2.val){
         head = l1;
-        if(l1.next === null){ // 如果l1已经用完了，就把l2都丢进去
-            head.next = l2;
-        }else{
-            head.next = mergeTwoLists(l1.next, l2);
-        }
+        head.next = mergeTwoLists(l1.next, l2);
     }else if(l1.val >= l2.val){
         head = l2;
-        if(l2.next === null){
-            head.next = l1;
-        }else{
-            head.next = mergeTwoLists(l1, l2.next);
-        }
+        head.next = mergeTwoLists(l1, l2.next);
     }
     return head;
 };
