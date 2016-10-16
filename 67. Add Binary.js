1bc6f880e51e5b220e8d65b5822c05bd01b8d419
余果 <fishguo321@gmail.com>
2016-10-16 10:59:20 +0800

    Update 67. Add Binary.js

--- 67. Add Binary.js
+++ 67. Add Binary.js
@@ -9,7 +9,7 @@ var addBinary = function(a, b) {
         carry = 0;
     for(let i = a.length, j = b.length; i > 0 || j > 0; i--, j--){
         let value = parseInt(a[i-1]||0) + parseInt(b[j-1]||0) + carry;
-        result = ((value%2) ? '1' : '0').concat(result);
+        result = ((value%2) ? '1' : '0') + result;
         carry = parseInt(value/2);
         console.log(carry);
     }    
@@ -18,3 +18,8 @@ var addBinary = function(a, b) {
     }
     return result;
 };
+
+// 坑在这里：
+// wrong:(value%2) ? '1' : '0' + result
+// right:((value%2) ? '1' : '0') + result
+// 运算的优先级不对了
