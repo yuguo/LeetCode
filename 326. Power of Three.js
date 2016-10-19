ff38c05a5ed281d7395cde3598746ff0620d771a
余果 <fishguo321@gmail.com>
2016-10-19 13:36:32 +0800

    不用循环或者递归，那就使用三进制

---
+++ 326. Power of Three.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfThree = function(n) {
+    var s = n.toString(3);
+    console.log(s.replace(/0/g, ''));
+    if(s[0] === '1' && s.replace(/0/g, '') === '1'){
+        return true;
+    }else{
+        return false;
+    }
+};
+// 不用循环或者递归，那就使用三进制
