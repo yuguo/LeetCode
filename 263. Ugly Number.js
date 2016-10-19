ea0ad0c636e3bc16e92995c62bb9720eea25089a
余果 <fishguo321@gmail.com>
2016-10-19 15:54:34 +0800

    Create 263. Ugly Number.js

---
+++ 263. Ugly Number.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var isUgly = function(num) {
+    if(num === 0){
+        return false;
+    }
+    while(num % 2 === 0){
+        num = num / 2;
+    }
+    while(num % 3 === 0){
+        num = num / 3;
+    }
+    while(num % 5 === 0){
+        num = num / 5;
+    }
+    if(num === 1){
+        return true;
+    }else{
+        return false;
+    }
+};
